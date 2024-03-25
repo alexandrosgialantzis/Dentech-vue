fc436c58dc7c17ca0aa0525cb7acd9941477aca0
Alexandros Gialantzis <alexandrosalexandrosg@gmail.com>
2024-03-25 21:42:55 +0200

    change urls

--- src/services/authHttp.ts
+++ src/services/authHttp.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3001/api/v1/auth'
+const API_URL = 'https://dentech-api.onrender.com/api/v1/auth'
 
 export const authHttp = axios.create({
   baseURL: API_URL,
