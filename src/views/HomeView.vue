0563712d8284d948f829e95950d2b7d86f5e4910
Alexandros Gialantzis <alexandrosalexandrosg@gmail.com>
2024-03-25 22:13:56 +0200

    add home image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,9 @@
 <template>
   <div class="d-flex justify-content-center align-items-center flex-column">
-    <h4>Καλωσορίσατε στο dentech!</h4>
-    <p>
-      Μέσα απο το dentech και βασή του ρολού σας, ο οποιός είναι
-      <span class="badge bg-primary">{{ user.role }}</span
-      >, μπορείτε να κάνετε τα εξής
-    </p>
-    <home-admin v-if="user.role === Roles.ADMIN" />
-    <home-user v-if="user.role === Roles.UNCATEGORIZED" />
-    <home-dentist v-if="user.role === Roles.DENTIST" />
+    <img
+      src="https://theapprenticedoctor.com/wp-content/uploads/2019/04/Depositphotos_178016396_XL-2048x1367.jpg"
+      alt=""
+    />
+    <div class="card p-3 rounded"><h4 class="mb-0">Καλωσορίσατε στο dentech!</h4></div>
   </div>
 </template>
-
-<script setup lang="ts">
-import { useUserStore } from '../stores/userStore'
-import { Roles } from '../types/enums'
-import HomeAdmin from '../components/home/HomeAdmin.vue'
-import HomeUser from '../components/home/HomeUser.vue'
-import HomeDentist from '../components/home/HomeDentist.vue'
-
-const { user } = useUserStore()
-</script>
-
-<style scoped>
-.card {
-  width: 100% !important;
-}
-</style>
