c024a83d0d36bd1790df84afbc42c8da96f57573
Alexandros Gialantzis <alexandrosalexandrosg@gmail.com>
2024-03-28 13:12:08 +0200

    vite env

--- src/services/orderHttp.ts
+++ src/services/orderHttp.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
-const API_URL = import.meta.env.VUE_APP_API_URL || 'https://dentech-api.onrender.com/api/v1/order'
+const API_URL = import.meta.env.VITE_API_URL || 'https://dentech-api.onrender.com/api/v1/order'
+console.log(import.meta.env.VITE_API_URL)
 
 export const orderHttp: AxiosInstance = axios.create({
   baseURL: API_URL,
--- src/services/productHttp.ts
+++ src/services/productHttp.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-const API_URL = import.meta.env.VUE_APP_API_URL || 'https://dentech-api.onrender.com/api/v1/product'
+const API_URL = import.meta.env.VITE_API_URL || 'https://dentech-api.onrender.com/api/v1/product'
 
 export const productHttp: AxiosInstance = axios.create({
   baseURL: API_URL,
