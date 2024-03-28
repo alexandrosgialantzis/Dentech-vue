5e38d8b0d19fef275e67bb27bf172e4cf61771be
Alexandros Gialantzis <alexandrosalexandrosg@gmail.com>
2024-03-28 13:00:49 +0200

    add env

--- src/services/authHttp.ts
+++ src/services/authHttp.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'https://dentech-api.onrender.com/api/v1/auth'
+const API_URL = import.meta.env.VITE_API_URL || 'https://dentech-api.onrender.com/api/v1/auth'
+console.log(import.meta.env.VITE_API_URL)
 
 export const authHttp = axios.create({
   baseURL: API_URL,
