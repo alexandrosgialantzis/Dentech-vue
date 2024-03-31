3dbdc3e3d800ee7398b32085691eed5bbe12ebf6
Alexandros Gialantzis <alexandrosalexandrosg@gmail.com>
2024-04-01 01:35:18 +0300

    log for url

--- src/services/productHttp.ts
+++ src/services/productHttp.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API_URL: string = import.meta.env.VITE_API_URL
   ? import.meta.env.VITE_API_URL + 'product'
-  : 'https://dentech-api.onrender.com/api/v1/product'
+  : '/api/v1/product'
 
 export const productHttp = axios.create({
   baseURL: API_URL,
